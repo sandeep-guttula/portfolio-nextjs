83233c60f042ea0100fc2decfba7dd563dea329c
sandeep-guttula <sandeepguttula2002@gmail.com>
2024-02-01 23:29:12 +0530

    added skills

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -10,7 +10,7 @@ const Skills = () => {
       <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
         Skills
       </p>
-      <div className=" rounded-md  flex flex-col antialiased  bg-[#0a0a0a] bg-dot-white/[0.2] items-center justify-center relative overflow-hidden">
+      <div className=" rounded-md w-[90%]  flex flex-col antialiased  bg-[#0a0a0a] bg-dot-white/[0.2] items-center justify-center relative overflow-hidden">
         <InfiniteMovingCards items={languages} direction="right" speed="slow" />
         <InfiniteMovingCards items={frameworks} direction="left" speed="slow" />
         <InfiniteMovingCards items={tools} direction="right" speed="slow" />
